a430e1dcd64b29f811650c3b790951c22d08e29f
AmirHossein Akbari <AkbariAmirhosseinx@gmail.com>
2023-05-02 20:05:27 +0430

    Migration of Primary Models Confirmes

--- MyApiServer/Program.cs
+++ MyApiServer/Program.cs
@@ -1,3 +1,6 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region DBContext
+
+builder.Services.AddDbContext<MyApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyApiConnection")));
+
+#endregion
 
 var app = builder.Build();
 
